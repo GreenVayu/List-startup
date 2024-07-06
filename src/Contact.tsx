a3ba7b16ccfd90b2307e0a32ec356f984f63003f
iamsohailbelim <sohailbelim2425@gmail.com>
2024-07-06 21:24:49 +0530

    mobile 3 sec done

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,5 +1,97 @@
-const Contact = () => {
-  return <h1>Under Construction</h1>;
-};
+import React from 'react'
 
-export default Contact;
+function Contact() {
+  return (
+    <div className="contactsbody lg:max-w-lg  lg:w-full">
+
+        <div className='mt-4'>
+        <img className="img h-36" src="/ourcoaches/33.jpg"/>
+        <div className="h-36 relative -top-36 bg-black opacity-60"></div>
+
+    <div className="relative -top-[16rem]">
+    <div className="text-amber-500 text-center font-semibold font-['Palanquin Dark']">OUR COACHES</div>
+    <div className=" text-xs text-center text-white font-normal font-['Orienta'] pt-2 px-10">Meet Our Professional Coaches who will train, help, and guide you step-by-step to launch your Low-Investment Startup in no time.</div>
+        </div>
+
+
+        </div>
+
+
+    <div className='sec2' >
+
+        <div className='flex flex-col'>
+
+        <div className=' -translate-y-36 flex justify-center items-center'>
+        <img className='h-40 w-auto'  src='/ourcoaches/abhi.jpg' alt='Abhishek'/>
+        <div className=" translate-y-28 px-4 absolute text-center text-sm text-emerald-800 font-normal font-['Orienta']">Start-up Coach, Entrepreneur, Founder GreenVayu, LIST</div>
+        <div className=" translate-y-36 px-4 pt-4 absolute text-center text-xl text-emerald-800 font-semibold font-['Mukta']">ABHISHEK RAI</div>
+        </div>
+
+        <div className=' -translate-y-8 flex justify-center items-center'>
+        <img className='h-40 w-auto'  src='/ourcoaches/rohit.png' alt='rohit'/>
+        <div className=" translate-y-28 px-4 absolute text-center text-sm text-emerald-800 font-normal font-['Orienta']">Start-up Coach, Entrepreneur, Founder GreenVayu, LIST</div>
+        <div className=" translate-y-36 px-4 pt-4 absolute text-center text-xl text-emerald-800 font-semibold font-['Mukta']">ROHIT GUPTA</div>
+        </div>
+
+        <div className='translate-y-20 flex justify-center items-center'>
+        <img className='h-40 w-auto'  src='/ourcoaches/richa.png' alt='richa'/>
+        <div className=" translate-y-28 px-4 absolute text-center text-sm text-emerald-800 font-normal font-['Orienta']">Start-up Coach, Entrepreneur, Founder GreenVayu, LIST</div>
+        <div className=" translate-y-36 px-4 pt-4 absolute text-center text-xl text-emerald-800 font-semibold font-['Mukta']">RICHA KHARE</div>
+        </div>
+
+        </div>
+
+        <div className='relative top-[22rem] flex flex-col'>
+
+        <div className=' -translate-y-36 flex justify-center items-center'>
+        <img className='h-40 w-auto'  src='/ourcoaches/niraj.jpg' alt='Niraj'/>
+        <div className=" translate-y-28 px-4 absolute text-center text-sm text-emerald-800 font-normal font-['Orienta']">Start-up Coach, Entrepreneur, Founder GreenVayu, LIST</div>
+        <div className=" translate-y-36 px-4 pt-4 absolute text-center text-xl text-emerald-800 font-semibold font-['Mukta']">NIRAJ SHRIVASTAVA</div>
+        </div>
+
+        <div className=' -translate-y-[2rem] flex justify-center items-center'>
+        <img className='h-40 w-auto'  src='/ourcoaches/sudeep.jpg' alt='Sudeep'/>
+        <div className=" translate-y-28 px-4 absolute text-center text-sm text-emerald-800 font-normal font-['Orienta']">Start-up Coach, Entrepreneur, Founder GreenVayu, LIST</div>
+        <div className=" translate-y-36 px-4 pt-4 absolute text-center text-xl text-emerald-800 font-semibold font-['Mukta']">SUDEEP SHRIVASTAVA</div>
+        </div>
+
+        </div>
+
+    </div>
+
+
+    <div className='sec3 mt-64'>
+    <div className='translate-y-40 pt-4'>
+    <div className="text-emerald-800 px-4 text-center text-xl font-semibold font-['Mukta']" >Our Purpose </div>
+    <div className="text-stone-500 text-base font-normal font-['Orienta'] py-4" >
+    At LIST Training Academy, we are proud to have a team of experienced and passionate coaches who are dedicated to helping you succeed<br/><br/>Our coaches bring a wealth of knowledge and a genuine passion for teaching. Get to know the experts who will guide you on your entrepreneurial journey.
+    </div>
+
+    <div className="mx-auto mt-2 bg-emerald-800 rounded-lg w-fit" > <button className="text-white text-base font-normal font-['Roboto'] px-4 py-4" >Contact us to know more </button></div>
+
+    </div>
+     
+
+    </div>
+
+
+
+
+
+{/*
+<div className="w-[542px] h-[326px] relative">
+  <div className="w-[542px] h-[233.17px] left-0 top-0 absolute">
+    <div className="w-[540.16px] left-[1.84px] top-[-0px] absolute text-emerald-800 text-[38px] font-semibold font-['Mukta'] tracking-widest">Our Purpose</div>
+    <div className="w-[542px] left-0 top-[78.17px] absolute text-stone-500 text-base font-normal font-['Orienta'] leading-normal tracking-wide">At LIST Training Academy, we are proud to have a team of experienced and passionate coaches who are dedicated to helping you succeed<br/><br/>Our coaches bring a wealth of knowledge and a genuine passion for teaching. Get to know the experts who will guide you on your entrepreneurial journey.</div>
+  </div>
+  <div className="w-[215.88px] h-12 px-7 py-3 left-0 top-[278px] absolute bg-emerald-800 rounded-lg flex-col justify-center items-center gap-4 inline-flex">
+    <div className="text-white text-base font-normal font-['Roboto']">Contact us to know more</div>
+  </div>
+</div>
+*/}
+
+    </div>
+  )
+}
+
+export default Contact
