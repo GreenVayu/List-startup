9ad89a8fe09590555d68f87478baaf37cbd0f68e
iamsohailbelim <sohailbelim2425@gmail.com>
2024-07-07 21:00:06 +0530

    sec5 of our story page made with responsiveness

--- src/Ourstory.tsx
+++ src/Ourstory.tsx
@@ -30,20 +30,20 @@ function Ourstory() {
 
 <div className="text-stone-500 text-base font-normal font-['Orienta'] leading-8 tracking-wide w-4/5 mx-auto mt-4 pl-4">LIST was created to empower individuals to pursue their entrepreneurial dreams with confidence.<br/>We understand the fear that can hold aspiring business owners back.<br/>LIST is here to discard any uncertainty and foster a space where dreams come true and you can make changes in the world. Let’s Innovate together.</div>
     
-<div className="mx-auto mt-6 bg-emerald-800 rounded-lg w-fit sm:pl-0 sm:ml-10" > <button className="text-white text-base font-normal font-['Roboto'] px-4 py-2" >Meet Our Coaches </button></div>
+<div className="mx-auto mt-6 bg-emerald-800 rounded-lg w-fit sm:pl-0 sm:ml-16" > <button className="text-white text-base font-normal font-['Roboto'] px-4 py-2" >Meet Our Coaches </button></div>
 
 </div>
 
 </div>
 
-<div className="sec3 mt-12 lg:w-[70vw]" >
+{/* <div className="sec3 mt-12" >
 
-<div>
-  <div className="text-xl font-semibold font-['Mukta'] tracking-widest w-fit mx-auto text-amber-500">How we work?</div>
+<div className="sm:bg-[url('/ourstory/cp.png')] sm:bg-cover sm:block sm:py-10">
+  <div className="text-xl font-semibold font-['Mukta'] tracking-widest w-fit mx-auto text-amber-500 sm:text-white">How we work?</div>
 
-  <div className="flex flex-col mt-4 w-fit ml-8 space-y-8 sm:flex-row sm:gap-10 sm:ml-0">
+  <div className="flex flex-col mt-4 w-fit mx-auto space-y-8 sm:flex-row sm:gap-8 ">
 
-  <div className="bg-white rounded-lg shadow px-6 py-8 mt-4 shadow-gray-500 w-72 mx-auto sm:mt-8 sm:w-48 sm:ml-8 lg:w-72 sm:ml-0">
+  <div className="bg-white rounded-lg shadow px-6 py-8 mt-4 sm:mt-8 sm:max-w-52 shadow-gray-500 mx-auto max-w-64 lg:min-w-72">
         <div className="bg-gradient-to-b from-amber-500 to-yellow-600 rounded-2xl w-fit">
           <img src='/ourstory/MdStackedBarChart.png' className="px-2 py-2"  />
         </div>
@@ -52,7 +52,7 @@ function Ourstory() {
         <div className="text-sm font-normal font-['Orienta'] leading-5 tracking-wide mt-2 pt-2" >We provide structured guidance to help you take the first step and navigate the complexities of starting a business.</div>
       </div>
 
-      <div className="bg-white rounded-lg shadow px-6 py-8 mt-4 shadow-gray-500 w-72 mx-auto sm:w-48 lg:w-72">
+      <div className="bg-white rounded-lg shadow px-6 py-8 mt-4 shadow-gray-500 mx-auto max-w-64 sm:max-w-56 lg:min-w-72">
         <div className="bg-gradient-to-b from-amber-500 to-yellow-600 rounded-2xl w-fit">
           <img src='/ourstory/Group.png' className="px-2 py-2"  />
         </div>
@@ -61,10 +61,7 @@ function Ourstory() {
         <div className="text-sm font-normal font-['Orienta'] leading-5 tracking-wide mt-2 pt-2" >Access customized resources and tools designed to meet your specific entrepreneurial needs.</div>
       </div>
 
-
-
-
-      <div className="bg-white rounded-lg shadow px-6 py-8 mt-4 shadow-gray-500 w-72 mx-auto sm:w-48 lg:w-72 ">
+      <div className="bg-white rounded-lg shadow px-6 py-8 mt-4 shadow-gray-500 mx-auto max-w-64 sm:max-w-52 lg:min-w-72">
         <div className="bg-gradient-to-b from-amber-500 to-yellow-600 rounded-2xl w-fit">
           <img src='/ourstory/MdPeopleAlt.png' className="px-2 py-2"  />
         </div>
@@ -78,8 +75,48 @@ function Ourstory() {
 </div>
 
 
+</div> */}
+
+<div className="sec5 mt-20">
+
+<div className="mb-4">
+  <div className="text-center text-emerald-800 text-xl font-semibold font-['Mukta'] tracking-widest sm:text-2xl" >Our Values</div>
+  <div className="text-center text-stone-500 text-xs font-normal font-['Orienta'] leading-6 tracking-wide sm:text-2xl">We are here only for you</div>
 </div>
 
+<div className="space-y-8 mt-8 ml-6 sm:flex sm:space-y-0 sm:space-x-4 lg:space-x-8">
+
+
+<div className="shadow shadow-gray-500 w-fit py-4 ">
+
+  <div className="bg-gradient-to-b from-amber-500 to-yellow-600 rounded-full w-fit px-2 py-2 mx-auto">
+    <img src="/ourstory/Vector.png"  />
+  </div>
+  <div className="text-black text-lg font-semibold font-['Mukta'] leading-5 tracking-wide w-fit mx-auto py-2" >Empowerment</div>
+  <div className="text-center text-stone-500 text-xs font-normal font-['Orienta'] leading-6 tracking-wide w-4/5 mx-auto" >We believe in equipping individuals with the knowledge & tools needed to take control of their entrepreneurial journey and achieve their goals.</div>
+</div>
+
+<div className="shadow shadow-gray-500 w-fit py-4 ">
+
+  <div className="bg-gradient-to-b from-amber-500 to-yellow-600 rounded-full w-fit px-2 py-2 mx-auto">
+    <img src="/ourstory/Vector1.png"  />
+  </div>
+  <div className="text-black text-lg font-semibold font-['Mukta'] leading-5 tracking-wide w-fit mx-auto py-2" >Support</div>
+  <div className="text-center text-stone-500 text-xs font-normal font-['Orienta'] leading-6 tracking-wide w-4/5 mx-auto" >We offer unwavering assistance and guidance, ensuring that entrepreneurs receive the encouragement and help necessary to navigate challenges and succeed.</div>
+</div>
+
+<div className="shadow shadow-gray-500 w-fit py-4 ">
+
+  <div className="bg-gradient-to-b from-amber-500 to-yellow-600 rounded-full w-fit px-2 py-2 mx-auto">
+    <img src="/ourstory/Vector2.png"  />
+  </div>
+  <div className="text-black text-lg font-semibold font-['Mukta'] leading-5 tracking-wide w-fit mx-auto py-2" >Education</div>
+  <div className="text-center text-stone-500 text-xs font-normal font-['Orienta'] leading-6 tracking-wide w-4/5 mx-auto" >Through our workshops, mentorship programs, and resources, we provide valuable learning opportunities that empower entrepreneurs to continuously develop their skills and knowledge.</div>
+</div>
+
+</div>
+
+</div>
         </div>
     )
 }
@@ -89,67 +126,59 @@ export default Ourstory
 
 {/* 
     
-<div className="w-[365.81px] h-[282px] relative">
-  <div className="w-[365.81px] h-[282px] left-0 top-0 absolute bg-white rounded-lg shadow" />
-  <div className="w-[261px] h-[175px] left-[52px] top-[46px] absolute">
-    <div className="w-[259px] h-[167px] left-[2px] top-[8px] absolute">
-      <div className="w-[259px] left-0 top-[103px] absolute text-stone-500 text-sm font-normal font-['Orienta'] leading-[18px] tracking-wide">Join a vibrant community of like-minded individuals who share your entrepreneurial spirit and are ready to support each other.</div>
-      <div className="left-0 top-[56px] absolute text-black text-[22px] font-semibold font-['Mukta'] leading-relaxed tracking-wide">Community Support</div>
-      <div className="w-9 h-9 left-[46px] top-0 absolute origin-top-left rotate-180 justify-center items-center inline-flex" />
-    </div>
-    <div className="w-[59.24px] h-[51px] left-0 top-0 absolute">
-      <div className="w-[59.24px] h-[51px] left-0 top-0 absolute bg-gradient-to-b from-amber-500 to-yellow-600 rounded-2xl" />
-      <div className="w-8 h-8 left-[14px] top-[10px] absolute" />
+<div className="w-[244.78px] h-[172px] relative">
+  <div className="w-[127.53px] left-[69.94px] top-[75px] absolute text-black text-lg font-semibold font-['Mukta'] leading-[18px] tracking-wide">Empowerment</div>
+  <div className="w-[244.78px] h-[172px] left-0 top-0 absolute">
+    <div className="w-[244.78px] left-0 top-[110px] absolute text-center text-stone-500 text-xs font-normal font-['Orienta'] leading-[18px] tracking-wide">We believe in equipping individuals with the knowledge & tools needed to take control of their entrepreneurial journey and achieve their goals.</div>
+    <div className="w-[49.37px] h-12 left-[97.70px] top-0 absolute">
+      <div className="w-[49.37px] h-12 left-0 top-0 absolute bg-gradient-to-b from-amber-500 to-yellow-600 rounded-full" />
     </div>
   </div>
 </div>
 
-----
 
-<div className="w-[1202px] h-[393px] justify-start items-start gap-[38px] inline-flex">
-  <div className="text-white text-[38px] font-semibold font-['Mukta'] tracking-widest">How we work?</div>
-  <div className="w-[1201.81px] h-[282px] relative">
-    <div className="w-[365.81px] h-[282px] left-0 top-0 absolute">
-      <div className="w-[365.81px] h-[282px] left-0 top-0 absolute bg-white rounded-lg shadow" />
-      <div className="w-[261px] h-[174px] left-[52px] top-[47px] absolute">
-        <div className="w-[259px] left-[2px] top-[110px] absolute text-stone-500 text-sm font-normal font-['Orienta'] leading-[18px] tracking-wide">We provide structured guidance to help you take the first step and navigate the complexities of starting a business.</div>
-        <div className="left-[2px] top-[63px] absolute text-black text-[22px] font-semibold font-['Mukta'] leading-relaxed tracking-wide">Step-by-Step Guidance</div>
-        <div className="w-[59.24px] h-[51px] left-0 top-0 absolute">
-          <div className="w-[59.24px] h-[51px] left-0 top-0 absolute bg-gradient-to-b from-amber-500 to-yellow-600 rounded-2xl" />
-          <div className="w-9 h-9 left-[48px] top-[7px] absolute origin-top-left rotate-180" />
+--------------------------
+
+
+
+<div className="w-[1038px] h-[419px] justify-center items-center gap-[38px] inline-flex">
+  <div className="w-[1045.96px] h-[310px] relative">
+    <div className="w-[1045.96px] h-[310px] left-0 top-0 absolute bg-white rounded-lg shadow" />
+    <div className="w-[919.46px] h-[261px] left-[62.74px] top-[27px] absolute">
+      <div className="w-[261px] h-[0px] left-[291.06px] top-0 absolute origin-top-left rotate-90 border border-stone-500/opacity-20"></div>
+      <div className="w-[261px] h-[0px] left-[628.40px] top-0 absolute origin-top-left rotate-90 border border-stone-500/opacity-20"></div>
+      <div className="w-[244.78px] h-[188px] left-[674.68px] top-[25px] absolute">
+        <div className="w-[244.78px] h-[113px] left-0 top-[75px] absolute">
+          <div className="w-[244.78px] left-0 top-[35px] absolute text-center text-stone-500 text-xs font-normal font-['Orienta'] leading-[14px] tracking-wide">Through our workshops, mentorship programs, and resources, we provide valuable learning opportunities that empower entrepreneurs to continuously develop their skills and knowledge.</div>
+          <div className="w-[87.42px] left-[79.19px] top-0 absolute text-black text-lg font-semibold font-['Mukta'] leading-[18px] tracking-wide">Education</div>
+        </div>
+        <div className="w-[49.37px] h-12 left-[97.70px] top-0 absolute">
+          <div className="w-[49.37px] h-12 left-0 top-0 absolute bg-gradient-to-b from-amber-500 to-yellow-600 rounded-full" />
         </div>
       </div>
-    </div>
-    <div className="w-[365.81px] h-[282px] left-[418px] top-0 absolute">
-      <div className="w-[365.81px] h-[282px] left-0 top-0 absolute bg-white rounded-lg shadow" />
-      <div className="w-[261px] h-[157px] left-[52px] top-[46px] absolute">
-        <div className="w-[259px] left-[2px] top-[111px] absolute text-stone-500 text-sm font-normal font-['Orienta'] leading-[18px] tracking-wide">Access customized resources and tools designed to meet your specific entrepreneurial needs.</div>
-        <div className="left-[2px] top-[64px] absolute text-black text-[22px] font-semibold font-['Mukta'] leading-relaxed tracking-wide">Tailored Resources</div>
-        <div className="w-[59.24px] h-[51px] left-0 top-0 absolute">
-          <div className="w-[59.24px] h-[51px] left-0 top-0 absolute bg-gradient-to-b from-amber-500 to-yellow-600 rounded-2xl" />
-          <div className="w-9 h-9 left-[48px] top-[8px] absolute origin-top-left rotate-180 justify-center items-center inline-flex" />
+      <div className="w-[244.78px] h-[174px] left-[337.34px] top-[25px] absolute">
+        <div className="w-[244.78px] h-[99px] left-0 top-[75px] absolute">
+          <div className="w-[244.78px] left-0 top-[35px] absolute text-center text-stone-500 text-xs font-normal font-['Orienta'] leading-[14px] tracking-wide">We offer unwavering assistance and guidance, ensuring that entrepreneurs receive the encouragement and help necessary to navigate challenges and succeed.</div>
+          <div className="w-[70.96px] left-[87.42px] top-0 absolute text-black text-lg font-semibold font-['Mukta'] leading-[18px] tracking-wide">Support</div>
+        </div>
+        <div className="w-[49.37px] h-12 left-[97.70px] top-0 absolute">
+          <div className="w-[49.37px] h-12 left-0 top-0 absolute bg-gradient-to-b from-amber-500 to-yellow-600 rounded-full" />
         </div>
       </div>
-      <div className="w-9 h-9 left-[64px] top-[54px] absolute">
-        <div className="w-[30px] h-[27px] left-[3px] top-[4.50px] absolute">
-        </div>
-      </div>
-    </div>
-    <div className="w-[365.81px] h-[282px] left-[836px] top-0 absolute">
-      <div className="w-[365.81px] h-[282px] left-0 top-0 absolute bg-white rounded-lg shadow" />
-      <div className="w-[261px] h-[175px] left-[52px] top-[46px] absolute">
-        <div className="w-[259px] h-[167px] left-[2px] top-[8px] absolute">
-          <div className="w-[259px] left-0 top-[103px] absolute text-stone-500 text-sm font-normal font-['Orienta'] leading-[18px] tracking-wide">Join a vibrant community of like-minded individuals who share your entrepreneurial spirit and are ready to support each other.</div>
-          <div className="left-0 top-[56px] absolute text-black text-[22px] font-semibold font-['Mukta'] leading-relaxed tracking-wide">Community Support</div>
-          <div className="w-9 h-9 left-[46px] top-0 absolute origin-top-left rotate-180 justify-center items-center inline-flex" />
-        </div>
-        <div className="w-[59.24px] h-[51px] left-0 top-0 absolute">
-          <div className="w-[59.24px] h-[51px] left-0 top-0 absolute bg-gradient-to-b from-amber-500 to-yellow-600 rounded-2xl" />
-          <div className="w-8 h-8 left-[14px] top-[10px] absolute" />
+      <div className="w-[244.78px] h-[172px] left-0 top-[25px] absolute">
+        <div className="w-[127.53px] left-[69.94px] top-[75px] absolute text-black text-lg font-semibold font-['Mukta'] leading-[18px] tracking-wide">Empowerment</div>
+        <div className="w-[244.78px] h-[172px] left-0 top-0 absolute">
+          <div className="w-[244.78px] left-0 top-[110px] absolute text-center text-stone-500 text-xs font-normal font-['Orienta'] leading-[18px] tracking-wide">We believe in equipping individuals with the knowledge & tools needed to take control of their entrepreneurial journey and achieve their goals.</div>
+          <div className="w-[49.37px] h-12 left-[97.70px] top-0 absolute">
+            <div className="w-[49.37px] h-12 left-0 top-0 absolute bg-gradient-to-b from-amber-500 to-yellow-600 rounded-full" />
+          </div>
         </div>
       </div>
     </div>
   </div>
+  <div className="relative">
+    <div className="left-0 top-0 absolute text-emerald-800 text-[38px] font-semibold font-['Mukta'] tracking-widest">Our Values</div>
+    <div className="left-[20px] top-[63px] absolute text-center text-stone-500 text-xs font-normal font-['Orienta'] leading-[14px] tracking-wide">We are here only for you</div>
+  </div>
 </div>
-
     */ }
